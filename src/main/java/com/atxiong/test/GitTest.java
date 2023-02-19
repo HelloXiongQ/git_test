7f2f15e6686b85366f2f4321e9fe6ab824019ebb
xiongTest <3092166831@qq.com>
2023-02-19 16:08:06 +0800

    pushTest提交

--- src/main/java/com/atxiong/test/GitTest.java
+++ src/main/java/com/atxiong/test/GitTest.java
@@ -3,5 +3,6 @@ package com.atxiong.test;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("我是测试Git");
+        System.out.println("push Test");
     }
 }
