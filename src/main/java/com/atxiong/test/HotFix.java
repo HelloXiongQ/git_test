ea7def220d5d9dc83b835dfbfea9cc0e27ba4daa
xiongTest <3092166831@qq.com>
2023-02-19 15:55:05 +0800

    master修改hot-fix提交

--- src/main/java/com/atxiong/test/HotFix.java
+++ src/main/java/com/atxiong/test/HotFix.java
@@ -3,5 +3,6 @@ package com.atxiong.test;
 public class HotFix {
     public static void main(String[] args) {
         System.out.println("我是hot-fix模块");
+        System.out.println("master修改的hot-fix模块");
     }
 }
