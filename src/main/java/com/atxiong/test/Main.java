0584ee319ff55dfd70ac28441b7077166ab4b3d1
HelloXiongQ <104003048+HelloXiongQ@users.noreply.github.com>
2023-02-19 16:16:07 +0800

    github第一次提交

--- src/main/java/com/atxiong/test/Main.java
+++ src/main/java/com/atxiong/test/Main.java
@@ -1,10 +1,20 @@
 package com.atxiong.test;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
+        
         System.out.println("hello world");
         System.out.println("haha");
         System.out.println("hello 3");
         System.out.println("hello hot-fix");
+        ArrayList<Integer> arr = new ArrayList<>();
+        
+        arr.add(2);
+        arr.add(12);
+        arr.add(32);
+        
+        System.out.println(arr);
+
     }
 }
